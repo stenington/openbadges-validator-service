7ae231390ca0b6f90fa24a4b3b356d50721bded5
Mike Larsson <mikelarssonftw@gmail.com>
2013-08-09 19:24:44 -0400

    Fully replicate smoke test script as cukes

--- features/step_definitions/smoke-test-steps.js
+++ features/step_definitions/smoke-test-steps.js
@@ -14,11 +14,13 @@ module.exports = function() {
       ASSERTIONS['0.5.0 assertion'] = JSON.stringify(examples.validOldAssertion(host));
       ASSERTIONS['1.0.0 assertion'] = JSON.stringify(examples.validAssertion(host));
       ASSERTIONS['1.0.0 signature'] = JSON.stringify(examples.validSignature(host));
+      ASSERTIONS['bad assertion'] = '{ "not": "an assertion at all" }';
+      ASSERTIONS['bad signature'] = 'randomstring';
       callback();
     });
   });
 
-  this.When(/^I paste in a properly formatted ([\d\.]+ \w+)$/, function(type, callback) {
+  this.When(/^I paste in a (.*)$/i, function(type, callback) {
     var browser = this.browser;
     browser.visit(this.url('/'), function(err){
       if (err)
@@ -33,16 +35,24 @@ module.exports = function() {
     });
   });
 
-  this.Then(/^I expect the validator to report that it's valid$/, function(callback) {
-    assert.equal(this.browser.text('.status'), 'Valid',
-                 "result status should show 'Valid'");
+  this.Then(/^I expect the validator to report that it's (valid|invalid)$/i, function(validity, callback) {
+    validity = validity.toLowerCase();
+    assert.equal(this.browser.text('.status').toLowerCase(), validity,
+                 "result status should show '" + validity + "'");
     callback();
   });
 
-  this.Then(/^show me that it's version ([\d\.]+)$/, function(version, callback) {
+  this.Then(/^show me that it's version ([\d\.]+)$/i, function(version, callback) {
     assert(this.browser.text('.version').indexOf(version) !== -1,
            "correct version displayed");
     callback();
   });
 
+  this.Then(/^show \w+ reason(?: "([^"]*)")?$/, function(reason, callback) {
+    assert(this.browser.query('.reason'), "reason element exists");
+    if (reason) 
+      assert(this.browser.text('.reason').match(new RegExp(reason, 'i')), 
+             "matches provided reason");
+    callback();
+  });
 };
